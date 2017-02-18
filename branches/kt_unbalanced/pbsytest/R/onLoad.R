c32b5280044a37bf5ddac6c27f92ce27d8946be4
kev (Kevin Tappe) <kevin.tappe@bwi.uni-stuttgart.de>
2017-02-18 14:59:07 +0000

    delete branch for unbalanced pbsytest (now unnecessary as incorporated in trunk in rev. 382)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plm@383 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/kt_unbalanced/pbsytest/R/onLoad.R
+++
@@ -1,5 +0,0 @@
-## onload message
-
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Behavior of 'plm::pbsystest(..., test=\"re\")' changed, statistic is now chisquared.\n To get old behavior of v1.4-0, use 'plm::pbsystest(..., test=\"re\", normal=TRUE)'")
-}
